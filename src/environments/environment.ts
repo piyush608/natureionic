e71b887b9c871b4ca7bff08a386a7e783e035323
Prashuram Kumar <webmaster2@meetingsnmore.com>
2019-12-19 16:42:32 +0530

    Changes in home, signin, signup, forgot pwd, onboading, recipe-card, bussiness card, product, recipe

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-const URL = "http://localhost:5000/api/v3";
+const URL = "https://temp.ntrhub.com/api/v3";
 
 export const environment = {
   production: false,
